3065f40866329bca192f5dc6076d7fb45cb7e1cd
BryanJenks <bryan.jenks@red-matter.com>
2014-01-10 23:57:40 -0500

    something somethinh

--- app.js
+++ app.js
@@ -28,6 +28,24 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// configure azure
+var nconf = require('nconf');
+nconf.env().defaults({
+    "AZURE_STORAGE_ACCOUNT": "defaultstorageacct5359",
+    "AZURE_STORAGE_ACCESS_KEY": "Qkss+PDZhXsOZzt9KHk1hbtHalGymQWewHQYDEPxwqC2xe2E1QDVCWfqzfQpngVg/Nt5jqUmEx9TTsdeTFjQQQ=="
+});
+
+var azure = require('azure');
+var storageAccount = nconf.get("AZURE_STORAGE_ACCOUNT");
+var storageKey = nconf.get("AZURE_STORAGE_ACCESS_KEY");
+var tableService = azure.createTableService(storageAccount, storageKey);
+tableService.createTableIfNotExists('people', function(error){
+    if (!error)
+    {
+        // table exists or was created
+    }
+})
+
 app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/api/test', function(req, res) {
@@ -35,6 +53,26 @@ app.get('/api/test', function(req, res) {
 });
 app.post('/api/test', function(req, res) {
     console.log(req.body);
+});
+app.get('/api/person/:id', function(req, res){
+    tableService.queryEntity('people', 'all', req.params.id,
+        function(error, entity){
+            if (error) { throw error; }
+            res.json({
+                "id": entity.RowKey,
+                "name": entity.Name
+            });
+        });
+});
+app.get('/api/person', function(req, res){
+    var person = {
+        PartitionKey: "all",
+        RowKey: req.body.id,
+        Name: req.body.name
+    };
+    tableService.insertEntity('people', person, function(error){
+        if (error) { throw error; }
+    });
 });
 
 http.createServer(app).listen(app.get('port'), function(){
